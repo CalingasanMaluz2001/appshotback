45d78f897fa00d5b7c50625ff594efe01e4c2eac
CalingasanMaluz2001 <calingasanmarialuz27@gmail.com>
2024-06-20 14:16:31 +0800

    first commit

---
+++ src/main/java/maluy/appshots/AppshotsApplication.java
@@ -0,0 +1,13 @@
+package maluy.appshots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppshotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppshotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maluy/appshots/AppshotsApplicationTests.java
@@ -0,0 +1,13 @@
+package maluy.appshots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppshotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
