101fe16eb595e5d929fbdd8f1f6d456279d06a88
Calingasanmaluz2001 <calingasanmarialuz27@gmail.com>
2024-06-25 16:35:51 +0800

    add get and post

---
+++ src/main/java/maluy/appshots/Model/Product.java
@@ -0,0 +1,56 @@
+package maluy.appshots.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    private @Id
+    @GeneratedValue Long id;
+    private String productName;
+    private String description;
+    private double price;
+
+    Product(){}
+    
+    public Product(String productName, String description, double price) {
+        this.productName = productName;
+        this.description = description;
+        this.price = price;
+    }
+
+    //setters
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    //Getters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+
+    
+}
---
+++ src/main/java/maluy/appshots/Repisotory/ProductRepisotory.java
@@ -0,0 +1,9 @@
+package maluy.appshots.Repisotory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import maluy.appshots.Model.Product;
+
+public interface ProductRepisotory extends JpaRepository<Product,Long>{
+
+}
