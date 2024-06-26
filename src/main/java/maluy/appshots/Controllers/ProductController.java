a4807bb9eee21c87fb394eb5018ee835137b85e8
CalingasanMaluz2001 <calingasanmarialuz27@gmail.com>
2024-06-26 16:31:18 +0800

    final activity 1

--- src/main/java/maluy/appshots/Controllers/ProductController.java
+++ src/main/java/maluy/appshots/Controllers/ProductController.java
@@ -2,40 +2,71 @@ package maluy.appshots.Controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import maluy.appshots.Model.Product;
+import maluy.appshots.NotFoundException.ProductNotFoundException;
 import maluy.appshots.Repisotory.ProductRepisotory;
 
 @RestController
 public class ProductController {
-    
-   
+
     ProductRepisotory repo;
 
     public ProductController(ProductRepisotory repo) {
         this.repo = repo;
     }
-    //http: //127.0.0.1/products
-    //Get all Products
-   @GetMapping("/products")
-    public List<Product> getProducts () {
+
+    // http: //127.0.0.1/products
+    // Get all Products
+    @GetMapping("/products")
+    public List<Product> getProducts() {
         return repo.findAll();
     }
+      
+    //http://127.0.0.1:8080/product/2
+    @GetMapping("/product/{id}")
+     public Product getProduct(@PathVariable Long id){
+     return repo.findById(id)
+     .orElseThrow(()->  new ProductNotFoundException(id));
+     }
 
-
-    // public Product getProduct(Long id){
-    //     return repo.findById(id;)
-    // }
-
-    //http: //127.0.0.1:8080/product/new
+    // http: //127.0.0.1:8080/product/new
     @PostMapping("/product/new")
-    public String addProduct(@RequestBody Product newProduct){
+    public String addProduct(@RequestBody Product newProduct) {
         repo.save(newProduct);
         return "A new Product is added. Yey!";
 
     }
+
+    // UPDATE ENDPOINTS
+    // http://127.0.0.1:8080/products/edit/1
+    @PutMapping("/product/edit/{id}")
+    public Product updateProduct(@PathVariable Long id,
+            @RequestBody Product newProduct) {
+        return repo.findById(id)
+                .map(product -> {
+                    product.setProductName(newProduct.getProductName());
+                    product.setDescription(newProduct.getDescription());
+                    product.setPrice(newProduct.getPrice());
+                    return repo.save(product);
+                }).orElseGet(() -> {
+                    return repo.save(newProduct);
+                });
+    }
+
+    // DELETE ENDPOINTS
+    // http://127.0.0.1:8080/products/delete/1
+    @DeleteMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        repo.deleteById(id);
+        return "A product is deleted";
+    }
+
 }
