27e6ced9a6764de818c2148adb00518bb7f7564c
CalingasanMaluz2001 <calingasanmaraialuz27@gmail.com>
2024-07-04 11:08:39 +0800

    new

---
+++ src/main/java/maluy/appshots/ExceptionHandler/OrderItemExceptionHandler.java
@@ -0,0 +1,18 @@
+package maluy.appshots.ExceptionHandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import maluy.appshots.NotFoundException.OrderItemNotFoundException;
+
+@RestControllerAdvice
+public class OrderItemExceptionHandler {
+
+    @ExceptionHandler(OrderItemNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String orderitemNotFoundHandler(OrderItemNotFoundException e){
+        return e.getMessage();
+    }
+}
